6f26db5cf53962eaaf018cb0b6d96aace15dfb54
Nitin Sharma <nsharma78@gmail.com>
2017-12-16 12:26:57 -0800

    PowerOfTwo implemented

--- src/main/java/com/nitin/algo/PowerOfTwo.java
+++ src/main/java/com/nitin/algo/PowerOfTwo.java
@@ -5,5 +5,22 @@
 package com.nitin.algo;
 
 public class PowerOfTwo {
+	
+	public static void main(String[] args) {
+		int n = 48;
+		System.out.println(n + " is power of 2: " + isPowerOfTwo(n));
+	}
+
+	private static boolean isPowerOfTwo(int n) {
+		if(n ==1 || n == 2)
+			return true;
+		
+		while(n > 2) {
+			if((n % 2) != 0)
+				return false;
+			n = n / 2;
+		}
+		return true;
+	}
 
 }
