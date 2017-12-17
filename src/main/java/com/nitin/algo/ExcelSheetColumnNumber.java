a62630ef22e978a160ad45c38ad40b6b4ce0e60e
Nitin Sharma <nsharma78@gmail.com>
2017-12-16 17:58:45 -0800

    ExcelSheetColumnNumber implemented

--- src/main/java/com/nitin/algo/ExcelSheetColumnNumber.java
+++ src/main/java/com/nitin/algo/ExcelSheetColumnNumber.java
@@ -14,5 +14,23 @@
 package com.nitin.algo;
 
 public class ExcelSheetColumnNumber {
+	
+	public static void main(String[] args) {
+		String input = "A";
+		System.out.println("Column number for the given string is: " + getNumber(input));
+	}
+
+	private static int getNumber(String input) {
+		if(input == null || input.isEmpty())
+			return 0;
+		
+		int num = 0;
+		
+		for(int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			num = num + (int) Math.pow(26, i) + (c - 'A');
+		}
+		return num;
+	}
 
 }
